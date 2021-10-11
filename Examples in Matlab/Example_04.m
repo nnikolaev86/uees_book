64482d9eb366ce955c5032b55ef720a22ce21aa4
Nikolay Nikolaev <47809698+nnikolaev86@users.noreply.github.com>
2021-10-11 11:50:37 +0300

    Updates

--- Examples in Matlab/Example_04.m
+++ Examples in Matlab/Example_04.m
@@ -1,11 +1,11 @@
-%% ПРИМЕР 3: Устойчивост при малки смущения на модел от 2-ри ред при вариращо Eq_prim
+%% ПРИМЕР 4: Устойчивост при малки смущения на модел от 2-ри ред при зададено Eq_prim
 clear all; clc; close all;
-fprintf('ПРИМЕР 3: Устойчивост при малки смущения на модел от 2-ри ред при вариращо Eq_prim\n')
+fprintf('ПРИМЕР 4: Устойчивост при малки смущения на модел от 2-ри ред при зададено Eq_prim\n')
 
 %% Входни данни
 
 % Режимни параметри на генератора и системата
-U1     = 1.02;
+Eq0_prim     = 1.1421;
 P1     = 0.95;
 
 U2     = 1.0;
@@ -18,18 +18,6 @@ w0 = 2*pi*50; % rad/s
 Sn = 500; % MVA
 H = 3.0;
 Xd_prim  = 0.3;
-Ra       = 0.0;
-
-% Собствени и взаимни проводимости без включен генераторен импеданс
-Y11  = 0.1882 -2.6990i ;
-Y22  = 0.1802 -2.8672i ;
-Y12  = 0.1407 -2.5663i ;
-Y21  = Y12;
-
-alpha_11 = pi/2 + angle(Y11);
-alpha_22 = pi/2 + angle(Y22);
-alpha_12 = pi/2 + angle(Y12);
-alpha_21 = pi/2 + angle(Y21);
 
 % Собствени и взаимни проводимости с включен генераторен импеданс
 % (изчислени са със скрипт Example_03_compute_admitances.m)
@@ -46,44 +34,14 @@ alpha_21g = pi/2 + angle(Y21g);
 % Коеф. за преобразуване от RAD в DEG
 toDeg = 180/pi; 
 
-%% a) Установен режим на мрежата
-fprintf('\na) Установен режим на мрежата\n')
-
-% Ъгъл на генераторното напрежение
-theta_10 = asin( (P1 - (U1^2)*abs(Y11)*sin(alpha_11))/(U1*U2*abs(Y12)) ) + alpha_12;
-Ug0 = U1*exp(1i*theta_10);
-fprintf('Ug0 = (%0.4f /_%0.4f deg) pu\n', abs(Ug0), angle(Ug0)*toDeg)
-
-% Мощности на генератора и системата
-P1 = (U1^2)*abs(Y11)*sin(alpha_11) + U1*U2*abs(Y12)*sin(theta_10 - alpha_12);
-Q1 = (U1^2)*abs(Y11)*cos(alpha_11) - U1*U2*abs(Y12)*cos(theta_10 - alpha_12);
-fprintf('P1      = %0.4f pu\n', P1)
-fprintf('Q1      = %0.4f pu\n', Q1)
-
-Ig0 = (P1 - 1i*Q1) / conj(Ug0);
-fprintf('Ig0 = %0.4f %+0.4fi pu \n', real(Ig0), imag(Ig0))
-
 %% b) Установен режим на генераторния модел от 2-ри ред
 fprintf('\nb) Установен режим на генераторния модел от 2-ри ред\n')
 
-delta0 = angle(Ug0 + (Ra + 1i*Xd_prim)*Ig0);
+% Роторният ъгъл този път се изчислява с ъгловата характеристика
+delta0 = asin( (P1 - (Eq0_prim^2)*abs(Y11g)*sin(alpha_11g))/(Eq0_prim*U2*abs(Y12g)) ) + alpha_12g;
 fprintf('delta0 = %0.4f deg\n', delta0 * toDeg)
 
-Id0 = real(Ig0)*sin(delta0) - imag(Ig0)*cos(delta0);
-Iq0 = real(Ig0)*cos(delta0) + imag(Ig0)*sin(delta0);
-fprintf('Id0 = %0.4f pu  Iq0 = %0.4f pu \n', Id0, Iq0)
-
-Ud0 = real(Ug0)*sin(delta0) - imag(Ug0)*cos(delta0);
-Uq0 = real(Ug0)*cos(delta0) + imag(Ug0)*sin(delta0);
-fprintf('Ud0 = %0.4f pu  Uq0 = %0.4f pu \n', Ud0, Uq0)
-
-Eq0_prim = Uq0 + Ra*Iq0 + Xd_prim*Id0;
-fprintf('Eq0_prim = %0.4f deg\n', Eq0_prim)
-
-psid0 = Eq0_prim - Xd_prim*Id0;
-psiq0 = -Xd_prim*Iq0;
-
-Pm0 = psid0*Iq0 - psiq0*Id0;
+Pm0 = P1;
 fprintf('Pm0 = %0.4f pu\n', Pm0)
 
 %% c) Линеаризиране на модела
